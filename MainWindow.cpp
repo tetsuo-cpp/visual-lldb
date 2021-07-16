b93cd0a05eb3a903b2516d7f9eab5bd9b3298ffb
Alex Cameron <ascottcameron@gmail.com>
2021-07-16 17:43:41 +1000

    Try to get the project building with LibLLDB

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,9 +1,35 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+
+#include <lldb/API/SBDebugger.h>
+
+namespace visual_lldb {
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
+  // Call this before manipulating the window.
   ui->setupUi(this);
+
+  // Create the debugger interface.
+  QTextEdit *textEdit = new QTextEdit;
+  textEdit->setReadOnly(true);
+  QPushButton *bpButton = new QPushButton("Set breakpoint");
+  QPushButton *runButton = new QPushButton("Run!");
+  QVBoxLayout *layout = new QVBoxLayout;
+  layout->addWidget(textEdit);
+  layout->addWidget(bpButton);
+  layout->addWidget(runButton);
+  QWidget *centralWidget = new QWidget;
+  centralWidget->setLayout(layout);
+  this->setCentralWidget(centralWidget);
+
+  lldb::SBDebugger dbg;
 }
 
 MainWindow::~MainWindow() { delete ui; }
+
+} // namespace visual_lldb
--- MainWindow.h
+++ MainWindow.h
@@ -6,13 +6,17 @@ namespace Ui {
 class MainWindow;
 }
 
+namespace visual_lldb {
+
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
 public:
-  explicit MainWindow(QWidget *parent = 0);
-  ~MainWindow();
+  explicit MainWindow(QWidget *parent = nullptr);
+  virtual ~MainWindow();
 
 private:
   Ui::MainWindow *ui;
 };
+
+} // namespace visual_lldb
